5740dfe8f99c786f25e45659252f76424e7c6c55
dontknow945 <a0978878897@gmail.com>
2021-12-10 20:38:41 +0800

    add a slider to change the number of squares per side

--- script.js
+++ script.js
@@ -3,11 +3,27 @@ const pad = 720;
 let size = 16;
 const body = document.querySelector('body');
 const clrbtn = document.querySelector('.clear');
+const size_range = document.querySelector('#size-range');
+const srlabel = document.querySelector('#srlabel');
 const container = document.createElement('div');
 let pixels = [];
 
 
 /* functions */
+function createDivs() {
+    for (let i=0; i<size; i++) {
+        for (let j=0; j<size; j++) {
+            pixels[i*size+j] = document.createElement('div');
+            pixels[i*size+j].style.width = pixels[i*size+j].style.height = `${pad/size}px`;
+            pixels[i*size+j].style.display = 'inline-block';
+            pixels[i*size+j].style.boxSizing = 'border-box';
+            pixels[i*size+j].style.border = '1px black solid';
+            container.appendChild(pixels[i*size+j]);
+            pixels[i*size+j].addEventListener('mouseover', changeColor);
+        }
+    }
+}
+
 function changeColor(e) {
     e.stopPropagation();
     this.style.backgroundColor = 'black';
@@ -19,29 +35,26 @@ function clearColor(e) {
     });
 }
 
+function changeSize(e) {
+    size = this.value;
+    srlabel.textContent = `size: ${size}`;
+    pixels.forEach(element => {
+        container.removeChild(element);
+    });
+    pixels.length = 0;
+    createDivs();
+}
 
-/* container */
+
+/* container settings */
 container.classList.add('container');
 container.style.width = container.style.height = `${pad}px`;
 container.style.display = 'flex';
 container.style.flexWrap = 'wrap';
 
 
-/* create square divs inside the container */
-for (let i=0; i<size; i++) {
-    for (let j=0; j<size; j++) {
-        pixels[i*size+j] = document.createElement('div');
-        pixels[i*size+j].style.width = pixels[i*size+j].style.height = `${pad/size}px`;
-        pixels[i*size+j].style.display = 'inline-block';
-        pixels[i*size+j].style.boxSizing = 'border-box';
-        pixels[i*size+j].style.border = '1px black solid';
-        container.appendChild(pixels[i*size+j]);
-        pixels[i*size+j].addEventListener('mouseover', changeColor);
-    }
-}
-
+createDivs();
 body.appendChild(container);
 
-
-/* clear button */
 clrbtn.addEventListener('click', clearColor);
+size_range.addEventListener('change', changeSize);
